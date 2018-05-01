1702f5fae2ab2518a42241cfe4a9e994c070aff0
Tristan Tyson(Coding) <tristantysoncoding@Tristans-MacBook-Pro.local>
2018-04-30 21:45:51 -0400

    parseLine eattempt

--- main.cpp
+++ main.cpp
@@ -46,12 +46,27 @@ int openFile(string fileLoc){
     if(!file) return -1;
 }
 
-int parseLine(string line, RocketData *data){
+int parseLine(string line, RocketData *data){ //explain pleas
     //parse the data and return a RocketData struct
     //if the data is not legit, return a null
+    // thank
+    string line = @{GX:5;GY:6;GZ:-1;TS:15;}@;
+    int gxStart = line.find("GX:") + 3;
+    int gyStart = line.find("GY:") + 3;
+    int gzStart = line.find("GZ:") + 3;
+    string gxValue = line.substr(gxStart, gyStart - 1 - gxStart);
+    string gyValue = line.substr(gyStart, gzStart - 1 - gyStart);
+    string gzValue = line.substr(gzStart, ??);
+    float gx = atof(gxValue.c_str());
+    float gy = atof(gyValue.c_ßstr());
+    float gz = atof(gzValue.c_str());
+    printf(gx);
+    printf(gy);
+    printf(gz);
     printf("Parsing New Line: %s\n", line.c_str());
 }
-
+//
+//
 RocketData findMaxAltitude(){
 
 }
